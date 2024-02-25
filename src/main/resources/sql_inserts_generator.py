39b02352a33903b3c744b793651c94a11302b4b1
xma4602 <xma4602@gmail.com>
2024-02-26 02:10:34 +0400

    В скриптах добавления тестовых данных в бд добавлены статус и тип для вопросов.

--- src/main/resources/sql_inserts_generator.py
+++ src/main/resources/sql_inserts_generator.py
@@ -16,16 +16,39 @@ class UUIDEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-rd = random.Random()
-rd.seed(0)
+rd_uuid = random.Random()
+rd_uuid.seed(0)
+
+rd_int = random.Random()
+rd_int.seed(0)
 
 
 def get_uuid():
-    return uuid.UUID(int=rd.getrandbits(128), version=4)
+    return uuid.UUID(int=rd_uuid.getrandbits(128), version=4)
+
+
+def get_count():
+    return rd_int.randint(0, 100)
+
+
+def get_type():
+    if rd_int.randint(0, 2) == 0:
+        return r"'SINGLE'"
+    else:
+        return r"'MULTI'"
+
+
+def get_status():
+    num = rd_int.randint(0, 3)
+    if num == 0:
+        return r"'EDIT_ONLY'"
+    elif num == 1:
+        return r"'ANSWERS_ONLY'"
+    else:
+        return r"'ARCHIVED'"
 
 
 data_path = r".\data.json"
-result_path = r".\result.json"
 questions_path = r".\sql\insert_questions.sql"
 answers_path = r".\sql\insert_answers.sql"
 surveys_path = r".\sql\insert_surveys.sql"
@@ -39,6 +62,7 @@ with open(data_path, 'r', encoding='utf-8') as file:
 
 surveys = []
 count = 10
+
 for n in range(len(data)):
     item = data[n]
     if n % count == 0:
@@ -51,6 +75,8 @@ for n in range(len(data)):
     question = {"question_id": question_id,
                 "survey_id": survey_id,
                 "topic": item['topic'],
+                "status": get_status(),
+                "type": get_type(),
                 "index": n % count,
                 "answers": []}
     survey["questions"].append(question)
@@ -59,12 +85,9 @@ for n in range(len(data)):
         answer = {"answer_id": get_uuid(),
                   "question_id": question_id,
                   "index": index,
-                  "count": 0,
+                  "count": get_count(),
                   "text": answers[index]}
         question["answers"].append(answer)
-
-with open(result_path, 'w', encoding='utf-8') as file:
-    json.dump(surveys, file, cls=UUIDEncoder)
 
 # ==============================================================================
 # чтение данных опросов и сохранение в SQL-insertы
@@ -76,10 +99,10 @@ surveys_file = open(surveys_path, 'w+', encoding='utf-8')
 surveys_file.write("INSERT INTO \"surveys\" (survey_id, title) VALUES\n")
 
 questions_file = open(questions_path, 'w+', encoding='utf-8')
-questions_file.write("INSERT INTO \"questions\" (question_id, survey_id, index, topic) VALUES\n")
+questions_file.write("INSERT INTO \"questions\" (question_id, survey_id, status, type, index, topic) VALUES\n")
 
 answers_file = open(answers_path, 'w+', encoding='utf-8')
-answers_file.write("INSERT INTO \"answers\" (answer_id, question_id, index, text, count) VALUES\n")
+answers_file.write("INSERT INTO \"answers\" (answer_id, question_id, count, index, text) VALUES\n")
 
 for survey in surveys:
     end = ';' if survey == surveys[-1] else ','
@@ -89,15 +112,16 @@ for survey in surveys:
     questions = survey["questions"]
     for question in questions:
         end = ';' if survey == surveys[-1] and question == questions[-1] else ','
-        s = "('{}', '{}', '{}', '{}')".format(question['question_id'], question['survey_id'],
-                                              question['index'], question['topic'])
+        s = "('{}', '{}', {:14}, {:8}, {}, '{}')".format(question['question_id'], question['survey_id'],
+                                                           question['status'], question['type'],
+                                                           question['index'], question['topic'])
         questions_file.write(s + end + '\n')
 
         answers = question["answers"]
         for answer in answers:
             end = ';' if survey == surveys[-1] and question == questions[-1] and answer == answers[-1] else ','
-            s = "('{}','{}', {}, '{}', {})".format(answer["answer_id"], answer["question_id"], answer['index'],
-                                               answer['text'], answer['count'])
+            s = "('{}', '{}', {:2}, {}, '{}')".format(answer["answer_id"], answer["question_id"],
+                                                    answer['count'], answer['index'], answer['text'])
             answers_file.write(s + end + '\n')
 
 surveys_file.close()
